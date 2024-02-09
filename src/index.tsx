a4d0762a6a43768472c4615df399a1d741093423
Andrew Hillier <ahillier88@live.com>
2024-02-09 21:35:58 +0000

    added basic bootstrap + router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HelloWorld from './HelloWorld';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HelloWorld />
+  },
+  {
+    path: "/app",
+    element: <App />
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
