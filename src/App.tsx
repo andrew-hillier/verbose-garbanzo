544b43a63eba22db1aa7f9a34568b5c9efdb0d1d
Andrew Hillier <ahillier88@live.com>
2024-02-10 12:37:53 +0000

    fixed href paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ function App() {
     <div>
       <nav className="navbar navbar-expand bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">MyApp</a>
+          <a className="navbar-brand" href="/">MyApp</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="navbar-item">
-                <a className="nav-link" href="#">Home</a>
+                <a className="nav-link" href="/">Home</a>
               </li>
             </ul>
           </div>
