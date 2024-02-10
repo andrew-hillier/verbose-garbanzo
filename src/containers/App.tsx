12c508113f90367497da73d84595909fb16ba7a0
Andrew Hillier <73840999+andrew-hillier@users.noreply.github.com>
2024-02-10 21:24:32 +0000

    accessing environment variables (#9)

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,21 +1,28 @@
 function App() {
+  const nodeEnv = process.env.NODE_ENV || "(null)";
+  const testVariable = process.env.REACT_APP_TEST_VARIABLE || "(null)";
+
   return (
     <div>
       <nav className="navbar navbar-expand bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">MyApp</a>
+          <a className="navbar-brand" href="/app">MyApp</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="navbar-item">
-                <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/app">Home</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
+
+      nodeEnv: {nodeEnv}
+      <br />
+      testVariable: {testVariable}
 
       <div id="content" className="container">
         <h2>Page Title</h2>
