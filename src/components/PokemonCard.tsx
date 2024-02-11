a997614f1b6acd3fa854807601de81cb48e64aa9
Andrew Hillier <ahillier88@live.com>
2024-02-11 12:45:01 +0000

    return collection from service, and added styling

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,16 @@
+import { Pokemon } from "../pokemon";
+
+function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{pokemon?.name}</h5>
+                <p className="card-text">
+                    {pokemon?.description}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default PokemonCard;
