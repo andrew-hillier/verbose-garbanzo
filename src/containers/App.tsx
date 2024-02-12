2a29c0da293456213a85523a3ceebd8464f33ba2
Andrew Hillier <ahillier88@live.com>
2024-02-12 21:24:23 +0000

    add cypress

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -60,7 +60,7 @@ function App() {
               <input className="form-control" id="emailAddressInput" placeholder="John Doe" />
             </div>
 
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-cy="launch-demo-modal-button">
               Launch demo modal
             </button>
 
@@ -69,15 +69,15 @@ function App() {
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
-                    <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <h1 className="modal-title fs-5" id="exampleModalLabel" data-cy="demo-modal-title">Modal title</h1>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div className="modal-body">
                     ...
                   </div>
                   <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" data-cy="demo-modal-close-button">Close</button>
+                    <button type="button" className="btn btn-primary" data-cy="demo-modal-submit-button">Save changes</button>
                   </div>
                 </div>
               </div>
