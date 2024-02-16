afb5f755583894005532131bd0a6d2dbd45e38bb
Andrew Hillier <ahillier88@live.com>
2024-02-16 13:40:51 +0000

    configure typescript linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
-export default [
-    {
-        "ignores": ["build/**"],
-        "rules": {
-            "semi": "error",
-            "prefer-const": "error"
-        }
-    }
-];
+import js from '@eslint/js'
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...ts.configs.stylistic,
+  {
+    ignores: [
+      'build/**',
+      'cypress.config.ts'
+    ],
+  }
+);
